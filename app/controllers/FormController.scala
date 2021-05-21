e001aaa8d4ae225d5b2b44c480d2bac33a52b9bb
Denis-Andrei <denispeptanariu@gmail.com>
2021-05-21 19:20:45 +0100

    simpleFormPost() updated to do a POST request

--- app/controllers/FormController.scala
+++ app/controllers/FormController.scala
@@ -1,29 +1,47 @@
 package controllers
 
+import models.Vehicle
 import play.api.data.Form
 import play.api.data.Forms.{mapping, number, text}
+import play.api.libs.json.{JsObject, Json}
+import play.api.libs.ws
+import play.api.libs.ws._
+import play.api.libs.ws.WSResponse
 import play.api.mvc.{AbstractController, AnyContent, ControllerComponents, MessagesAbstractController, MessagesControllerComponents, Request}
 
 import javax.inject._
+import scala.concurrent.{Await, ExecutionContext, Future}
 
 @Singleton
-class FormController  @Inject()(cc: MessagesControllerComponents) extends MessagesAbstractController(cc) with play.api.i18n.I18nSupport{
+class FormController  @Inject()(ws: WSClient,cc: MessagesControllerComponents, implicit val ec: ExecutionContext) extends MessagesAbstractController(cc) with play.api.i18n.I18nSupport{
 
   def simpleForm() = Action {  implicit request: Request[AnyContent] =>
     Ok(views.html.form(BasicForm.form))
   }
 
-  def simpleFormPost() = Action { implicit request =>
+  def simpleFormPost() = Action.async { implicit request =>
     val postData = request.body.asFormUrlEncoded
-    postData.map{args =>
-      val vehicleName = args("Vehicle Name").head
-      println(vehicleName.getClass.getSimpleName)
-      Ok(s" You've introduced the ${vehicleName}")
+
+    val vehicleName = postData.map{args =>
+      args("Vehicle Name").head
     }.getOrElse(Ok("Error"))
 
+    val dataToBeSend = Json.obj(
+      "Vehicle Name" -> s"${vehicleName}"
+    )
+
+    val futureResponse: Future[WSResponse] = ws.url("http://localhost:9001/form").post(dataToBeSend)
+
+    futureResponse.map {
+      response =>
+        val js = Json.fromJson[Vehicle](response.json)
+        val veh = js.get
+        Ok(views.html.vehicle(veh))
+    }recover {
+      case _ => NotFound
+    }
+
   }
-
-
 }
 
 case class BasicForm(name: String)
